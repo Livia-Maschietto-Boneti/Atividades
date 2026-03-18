b68e076ca2495bd3a506e2f3451e3d3923e9f959
Livx <edwardghostiie@gmail.com>
2026-03-18 19:55:33 -0300

    primeiro commit

---
+++ src/test/java/liveduda/ativ/AtivApplicationTests.java
@@ -0,0 +1,13 @@
+package liveduda.ativ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtivApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
